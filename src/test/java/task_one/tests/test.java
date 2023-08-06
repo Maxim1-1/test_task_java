34c95b10f70de60898c31658738d26ec35d36fe4
Maxim1-1 <kick52@yadnex.ru>
2023-08-06 22:33:31 +0300

    add junit

--- src/test/java/task_one/tests/test.java
+++ src/test/java/task_one/tests/test.java
@@ -1,4 +1,16 @@
 package task_one.tests;
 
+import org.junit.Test;
+import task_one.cards.BaseCard;
+import task_one.cards.CreditCard;
+import task_one.cards.DebitCard;
+
 public class test {
+    @Test
+    public void s () {
+//        BaseCard creditCard = new CreditCard();
+        DebitCard debitCard = new DebitCard();
+
+
+    }
 }
